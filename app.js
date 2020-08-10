71e07d582399e2bd8a25581f1b3a52fba6033833
Laura <flaura42@gmail.com>
2020-08-10 00:16:44 -0700

    refactor: improved count updating and event listeners

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
-const kittens = [
-  {
-    name: 'Chloe',
-    src: './kittens/kitten1.jpg',
-    count: 0
-  },
-  {
-    name: 'Scaredy',
-    src: './kittens/kitten2.jpg',
-    count: 0
-  },
-  {
-    name: 'Bubbles',
-    src: './kittens/kitten3.jpg',
-    count: 0
-  },
-  {
-    name: 'Fluffy',
-    src: './kittens/kitten4.jpg',
-    count: 0
-  },
-  {
-    name: 'Tiger',
-    src: './kittens/kitten5.jpg',
-    count: 0
-  },
-  {
-    name: 'Angel',
-    src: './kittens/kitten6.jpg',
-    count: 0
-  }
-]
+const kittenList = {
+  currentKitten: null,
+  kittens: [
+    {
+      name: 'Chloe',
+      src: './kittens/kitten1.jpg',
+      count: 0
+    },
+    {
+      name: 'Scaredy',
+      src: './kittens/kitten2.jpg',
+      count: 0
+    },
+    {
+      name: 'Bubbles',
+      src: './kittens/kitten3.jpg',
+      count: 0
+    },
+    {
+      name: 'Fluffy',
+      src: './kittens/kitten4.jpg',
+      count: 0
+    },
+    {
+      name: 'Tiger',
+      src: './kittens/kitten5.jpg',
+      count: 0
+    },
+    {
+      name: 'Angel',
+      src: './kittens/kitten6.jpg',
+      count: 0
+    }
+  ]
+}
 
 window.addEventListener('load', () => { loadKittens(); })
 
-const thumbs = document.getElementById('kitten-thumbs');
-
 const loadKittens = () => {
-  kittens.forEach(kitten => {
+  const thumbs = document.getElementById('kitten-thumbs');
+  kittenList.kittens.forEach(kitten => {
     const figure = document.createElement('figure');
     figure.className = 'figure-thumb';
 
     const img = document.createElement('img');
     img.className = 'kitten-thumb';
     img.src = kitten.src;
+    img.addEventListener('click', () => {
+      kittenList.currentKitten = kitten;
+      addKitten();
+    })
     figure.append(img);
 
     const caption = document.createElement('figcaption');
@@ -51,30 +57,27 @@ const loadKittens = () => {
     figure.append(caption);
 
     thumbs.append(figure);
-  })
-    addKitten(kittens[0]);
+  });
+  kittenList.currentKitten = kittenList.kittens[0];
+  addKitten();
 }
 
-thumbs.addEventListener('click', (e) => {
-  const kName = e.target.nextSibling.innerHTML;
-  const kitten = kittens.find(kit => { return kit.name === kName; });
-  addKitten(kitten);
-})
-
-const addKitten = (kitten) => {
+const addKitten = () => {
+  const kitten = kittenList.currentKitten;
   const caption = document.getElementById('caption');
   caption.innerHTML = kitten.name;
 
   const img = document.getElementById('kitten-img');
-  img.addEventListener('click', () => { updateCount(kitten); })
   img.src = kitten.src;
+  img.addEventListener('click', updateCount);
 
   const count = document.getElementById('count');
   count.innerText = kitten.count;
 }
 
-const updateCount = (kitten) => {
-  kitten.count = kitten.count + 1;
+const updateCount = () => {
+  kitten = kittenList.currentKitten;
+  kittenList.currentKitten.count++;
   const kCount = document.getElementById('count');
-  kCount.innerText = kitten.count;
+  kCount.innerText = kittenList.currentKitten.count;
 }
